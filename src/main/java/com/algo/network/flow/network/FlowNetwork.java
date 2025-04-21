e7b9a742569480de0303bd2b000751cece34fabb
Soshan Wijayarathne <74235465+SoshanW@users.noreply.github.com>
2025-04-21 21:45:36 +0530

    created FlowNetwork.java with numberOfNodes, sourceNode, sinkNode, and adjacencyList as private fields and added Constructor, getter methods and toString Method

--- src/main/java/com/algo/network/flow/network/FlowNetwork.java
+++ src/main/java/com/algo/network/flow/network/FlowNetwork.java
@@ -1,13 +1,64 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+/**
+ * Represents a flow network using adjacency lists.
+ * Author: Soshan Wijayarathne
+ * IIT id: 20230427
+ * UOW id: w2051662
  */
 package com.algo.network.flow.network;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- *
- * @author user
+ * The Flow Network class represnts a directed graph for network flow problems
  */
 public class FlowNetwork {
-    
+    private final int numberOfNodes;
+    private final int sourceNode;
+    private final int sinkNode;
+    private final List<List<Edge>> adjacencyList;
+
+
+    /**
+     * Constructs a flow network with the specified number of nodes, source, and sink.
+     *
+     * @param numberOfNodes the total number of nodes
+     * @param sourceNode the source node index
+     * @param sinkNode the sink node index
+     */
+    public FlowNetwork(int numberOfNodes, int sourceNode, int sinkNode) {
+        this.numberOfNodes = numberOfNodes;
+        this.sourceNode = sourceNode;
+        this.sinkNode = sinkNode;
+        this.adjacencyList = new ArrayList<>();
+        for (int i = 0; i < numberOfNodes; i++) {
+            adjacencyList.add(new ArrayList<>());
+        }
+    }
+
+    public int getNumberOfNodes() {
+        return numberOfNodes;
+    }
+
+    public int getSourceNode() {
+        return sourceNode;
+    }
+
+    public int getSinkNode() {
+        return sinkNode;
+    }
+
+    public List<List<Edge>> getAdjacencyList() {
+        return adjacencyList;
+    }
+
+    @Override
+    public String toString() {
+        return "FlowNetwork{" +
+                "numberOfNodes=" + numberOfNodes +
+                ", sourceNode=" + sourceNode +
+                ", sinkNode=" + sinkNode +
+                ", adjacencyList=" + adjacencyList +
+                '}';
+    }
 }
