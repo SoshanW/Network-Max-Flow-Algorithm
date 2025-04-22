45a7e47ba559fa94d81b911df932712bce353cbf
Soshan Wijayarathne <74235465+SoshanW@users.noreply.github.com>
2025-04-22 17:52:47 +0530

    Implemented the main method to run to the program

--- src/main/java/com/algo/network/flow/Main.java
+++ src/main/java/com/algo/network/flow/Main.java
@@ -1,13 +1,35 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+/**
+ * Entry point for the maximum network flow application
+ * Author: Soshan Wijayarathne
+ * IIT id: 20230427
+ * UOW id: w2051662
  */
 package com.algo.network.flow;
 
-/**
- *
- * @author user
- */
+
+import com.algo.network.flow.algorithm.FordFulkerson;
+import com.algo.network.flow.network.FlowNetwork;
+import com.algo.network.flow.parser.Parser;
+
+
 public class Main {
-    
+
+    /**
+     * Main method to run the program
+     *
+     * @param args command-line arguments
+     */
+    public static void main(String[] args) {
+        String filename = "network.txt";
+
+        try{
+            FlowNetwork flowNetwork = Parser.parseFlowNetwork(filename);
+            System.out.println("Calculating maximum flow...");
+            int maxFlow = FordFulkerson.computeMaximumFlow(flowNetwork);
+            System.out.println("Maximum flow: " + maxFlow);
+        } catch (Exception e) {
+            System.out.println("ERROR: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
 }
