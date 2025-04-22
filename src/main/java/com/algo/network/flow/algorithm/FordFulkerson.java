5c5f7295441eaf99cb5f331883714e169daafdb2
Soshan Wijayarathne <74235465+SoshanW@users.noreply.github.com>
2025-04-22 13:19:11 +0530

    Implemented initial skeleton for the FordFulkerson method Edmonds Karp Algorithm

--- src/main/java/com/algo/network/flow/algorithm/FordFulkerson.java
+++ src/main/java/com/algo/network/flow/algorithm/FordFulkerson.java
@@ -1,13 +1,32 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+/**
+ * Implements the Ford-Fulkerson method Edmonds-Karp Algorithm for maximum flow
+ * Author: Soshan Wijayarathne
+ * IIT id: 20230427
+ * UOW id: w2051662
  */
 package com.algo.network.flow.algorithm;
 
+
+import com.algo.network.flow.network.FlowNetwork;
+
+import java.util.List;
+
 /**
- *
- * @author user
+ * The FordFulkerson class provides a method to compute the maximum flow in a  flow network using the Edmonds-Karp algorithm
  */
 public class FordFulkerson {
-    
+
+    public static int computeMaximumFlow(FlowNetwork network){
+        int maxFlow = 0;
+        List<Integer> augmentingPath;
+
+        while((augmentingPath = findAugementingPath(network))!=null){
+            //have to find bottleneck
+            //update the redisual path
+            //add bottle neck to the maxflow
+            //print augmenting path
+        }
+    }
+
+
 }
